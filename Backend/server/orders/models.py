752610f2eaa8d68fdc15f3bc9d74bb377cd91c2d
SergeG28071988 <sergeygalckin2012@yandex.ru>
2024-09-10 10:03:37 +0300

    Создали модель , зарегистрировали ее, выполнили миграции

--- Backend/server/orders/models.py
+++ Backend/server/orders/models.py
@@ -1,3 +1,38 @@
 from django.db import models
-
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 # Create your models here.
+
+
+class Order(models.Model):
+    objects = None
+
+    photo = models.ImageField(upload_to='uploads/images',
+                              help_text="Введите фото",
+                              verbose_name="Фото товара",
+                              null=False, blank=False)
+
+    order_number = models.CharField(max_length=100, verbose_name='Номер заказа', null=False, blank=False)
+    client = models.CharField(max_length=100, related_name='orders', verbose_name='Клиент')
+    date_created = models.DateField(verbose_name='Дата создания')
+    ORDER_STATUS_CHOICES = [
+        ('Поступил', 'Поступил'),
+        ('Выполняется', 'Выполняется'),
+        ('Закрыт', 'Закрыт'),
+    ]
+    status = models.CharField(max_length=50, choices=ORDER_STATUS_CHOICES, verbose_name='Статус')
+    completed = models.BooleanField(default=False, verbose_name='Заказ завершен')
+    completion_date = models.DateField(null=True, blank=True, verbose_name='Дата завершения')
+
+    def __str__(self):
+        return f'{self.order_number} {self.client}'
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+@receiver(pre_save, sender=Order)
+def update_completion_date(sender, instance, **kwargs):
+    if instance.completed and not instance.completion_date:
+        instance.completion_date = instance.date_created
+        
--- Backend/server/server/settings.py
+++ Backend/server/server/settings.py
@@ -55,11 +55,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'server.urls'
-
+TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -108,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
@@ -121,7 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR / "static"),
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
