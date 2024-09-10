8511dd511ed5e1482f517b0e1424f1d23f445734
SergeG28071988 <sergeygalckin2012@yandex.ru>
2024-09-10 10:54:56 +0300

    Тест проекта

--- Backend/server/orders/admin.py
+++ Backend/server/orders/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
-
+from .models import Order
+from django.utils.html import format_html, mark_safe
 # Register your models here.
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('show_photo', 'order_number', 'client', 'name', 'category', 'date_created')
+    list_filter = ('order_number', 'client')
+    readonly_fields = ["show_photo"]
+
+    def show_photo(self, obj):
+        if obj.photo:
+            return format_html(
+                f'<img src="{obj.photo.url}" style="max-height: 100px;">')
+            # можно и с использованием функции mark_safe
+            # return mark_safe(
+            # f'<img src="{obj.photo.url}" style="max-height: 100px;">')
+        else:
+            return "Фото не доступно"
+
+    show_photo.short_description = 'Фото товара'
+    
--- Backend/server/orders/models.py
+++ Backend/server/orders/models.py
@@ -10,10 +10,21 @@ class Order(models.Model):
     photo = models.ImageField(upload_to='uploads/images',
                               help_text="Введите фото",
                               verbose_name="Фото товара",
-                              null=False, blank=False)
+                              null=False, blank=False)  
 
-    order_number = models.CharField(max_length=100, verbose_name='Номер заказа', null=False, blank=False)
-    client = models.CharField(max_length=100, related_name='orders', verbose_name='Клиент')
+    order_number = models.CharField(max_length=100, verbose_name='Номер заказа', null=False, blank=False)    
+    client = models.CharField(max_length=100, verbose_name='Клиент', null=False, blank=False)
+    name = models.CharField(max_length=100, help_text="Введите товар", verbose_name="Товар", null=False, blank=False)
+    CATEGORY_CHOICES = [
+        ('Процессоры', 'Процессоры'),
+        ('Материнские платы', 'Материнские платы'),
+        ('Видеокарты', 'Видеокарты'),
+        ('Оперативная память', 'Оперативная память'),
+        ('SSD-диски', 'SSD-диски'),
+        ('HDD-диски', 'HDD-диски'),
+        ('Блоки питания>', 'Блоки питания'),
+    ]
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, verbose_name='Категория', null=True, blank=True)
     date_created = models.DateField(verbose_name='Дата создания')
     ORDER_STATUS_CHOICES = [
         ('Поступил', 'Поступил'),
@@ -35,4 +46,3 @@ class Order(models.Model):
 def update_completion_date(sender, instance, **kwargs):
     if instance.completed and not instance.completion_date:
         instance.completion_date = instance.date_created
-        
